d04371be0d357fd177c6f5c955b79b7ac84737de
bababoss <suresheca2012@gmail.com>
2020-03-27 12:20:23 +0000

    add notebook

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route, list_route
+#from rest_framework.decorators import detail_route, list_route
 from rest_framework.parsers import FileUploadParser, MultiPartParser,JSONParser
 
 ########################## import   Model #################################
